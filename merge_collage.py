2bdd87fa5dc319d3ec48dd268b2821b18f737396
tameemrok <debatewhatif@gmail.com>
2025-05-04 16:48:15 +0530

    Updated merge_collage.py

--- merge_collage.py
+++ merge_collage.py
@@ -1,24 +1,12 @@
-from PIL import Image, ImageDraw, ImageFont
-import requests
+from PIL import Image
 from io import BytesIO
+import requests
 
-def create_collage(image_urls, labels, output_path="static/collage.jpg"):
-    assert len(image_urls) == 4 and len(labels) == 4
-
+def create_collage(image_urls, eras, output_path="static/collage.jpg"):
     images = [Image.open(BytesIO(requests.get(url).content)).resize((512, 512)) for url in image_urls]
-    collage = Image.new("RGB", (1024, 1024))
 
-    try:
-        font = ImageFont.truetype("arial.ttf", 28)
-    except:
-        font = ImageFont.load_default()
-
-    draw_positions = [(0, 0), (512, 0), (0, 512), (512, 512)]
-
+    collage = Image.new("RGB", (len(images) * 512, 512))
     for i, img in enumerate(images):
-        collage.paste(img, draw_positions[i])
-        draw = ImageDraw.Draw(collage)
-        draw.text((draw_positions[i][0]+10, draw_positions[i][1]+10), labels[i], fill="white", font=font)
+        collage.paste(img, (i * 512, 0))
 
     collage.save(output_path)
-    return output_path
